021c6591f77fee16396edad5b69769545bbcbb8a
WinHack9702 <114675009+WinHack9702GitHub@users.noreply.github.com>
2022-11-08 19:45:51 +0300

    Update setup.bat

--- sds.am/setup.bat
+++ sds.am/setup.bat
@@ -9,8 +9,8 @@ if "%sdsamsetsel%"=="2" (exit)
 :setup
 wget --no-check-certificate "https://raw.githubusercontent.com/WinHack9702GitHub/sdos-appstore/main/sds.am/appmaker.bat"
 copy appmaker.bat C:\Windows\appmaker.bat
-echo echo sdsappmak - SysDataSoft App maker >> C:\Windows\appman\name.cmd
-echo if "%appmansel%"=="sdsappmak" (start C:\Windows\appmaker.bat) >> C:\Windows\appman\if.cmd
+echo echo appmaker - SysDataSoft App maker >> C:\Windows\appman\name.cmd
+echo if "%appmansel%"=="appmaker" (start C:\Windows\appmaker.bat) >> C:\Windows\appman\if.cmd
 cls
 echo Done!
 pause >> nul
