73347bb3e323e483f18a6b34625e8f4d4a9c877b
WinHack9702 <114675009+WinHack9702GitHub@users.noreply.github.com>
2022-11-08 18:31:27 +0300

    Create clnr.bat

---
+++ sds.clnr/clnr.bat
@@ -0,0 +1,15 @@
+@echo off
+echo =========Cleaner===========
+echo =Select:                  =
+echo =1.Clear Temp             =
+echo =2.exit                   =
+echo ===========================
+set /p selclnr=Select:
+if "%selclnr%"=="1" (goto dsa)
+exit
+:dsa
+del C:\Windows\setup.bat
+cls
+echo Done!
+paue >> nul
+exit
