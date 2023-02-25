25044cf68601a3c4d1cbcb8a2a53e35b70bdbe4f
WinHack9702 <114675009+WinHack9702GitHub@users.noreply.github.com>
2023-02-25 20:20:56 +0300

    Add files via upload

--- sds.am/appmaker.bat
+++ sds.am/appmaker.bat
@@ -6,12 +6,10 @@ echo Select:
 echo 1 - Create console app (Batch)
 echo 2 - Create GUI app (HTML)
 echo 3 - Create GUI app (VBS)
-echo 4 - Create console app (SysDataScript)
 set /p selsdsam=Select:
 if "%selsdsam%"=="1" (goto batch)
 if "%selsdsam%"=="2" (goto html)
 if "%selsdsam%"=="3" (goto vbs)
-if "%selsdsam%"=="4" (goto sdscr)
 echo Incorrect!
 pause
 goto app
@@ -64,24 +62,6 @@ copy %appaltnamevbs%.bat C:\SDOSApps\%appidvbs%\%appaltnamebatch%.bat
 echo echo %appaltnamevbs% - %appnamevbs% >> C:\Windows\appman\name.cmd
 echo if "%appmansel%"=="%appaltnamevbs%" (start C:\SDOSApps\%appidvbs%\%appaltnamevbs%.bat) >> C:\Windows\appman\if.cmd
 echo start C:\SDOSApps\%appidvbs%\%appaltnamevbs%.vbs >> C:\SDOSApps\%appidvbs%\%appaltnamevbs%.bat
-cls
-echo Done!app is created and installed.Click any key to return to app window...
-pause >> nul
-goto app
-:sdscr
-cls
-echo Create SysDataScript App
-set /p appnamesdscr=Enter App name:
-set /p appidsdscr=Enter App ID:
-set /p appaltnamesdscr=Enter App Alternate Name (no spaces,dots,only numbers and english letters):
-md C:\SDOSApps\%appidsdscr%
-cls
-echo Enter Code,when you ended,click Ctrl+Z.
-copy con tmp2.bat
-copy /b sysdatascript.bat + tmp2.bat %appaltnamesdscr%.bat
-copy %appaltnamesdscr%.bat C:\SDOSApps\%appidsdscr%\%appaltnamesdscr%.bat
-echo echo %appaltnamesdscr% - %appnamesdscr% >> C:\Windows\appman\name.cmd
-echo if "%appmansel%"=="%appaltnamesdscr%" (start C:\SDOSApps\%appidsdscr%\%appaltnamesdscr%.bat) >> C:\Windows\appman\if.cmd
 cls
 echo Done!app is created and installed.Click any key to return to app window...
 pause >> nul
